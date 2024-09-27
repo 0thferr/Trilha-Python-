f19287edc8bc68c504fab228c997a3c87b568dc5
Thaís Ferreira <152410711+0thferr@users.noreply.github.com>
2024-09-27 20:15:51 -0300

    Delete Dia 6/05 directory

--- Dia 6/05/config.py
+++
@@ -1,4 +0,0 @@
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'usuario_2'
-MYSQL_PASSWORD = '100901'
-MYSQL_DB = 'banco_teste'
--- Dia 6/05/tabela.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE minha_base_de_dados;
-USE minha_base_de_dados;
-
-CREATE TABLE dados_api (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100),
-    valor INT
-);
